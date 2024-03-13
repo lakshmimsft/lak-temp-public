be8fd5fbaebb65a01bbca33bc35fea1a25033c3b
Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>
2024-03-13 09:05:52 -0700

    Update main.tf

--- azuredevops/main.tf
+++ azuredevops/main.tf
@@ -7,13 +7,6 @@ terraform {
   }
 }
 
-provider "azuredevops" {
-  org_service_url = "https://dev.azure.com/my-org"
-  client_id     = "00000000-0000-0000-0000-000000000001"
-  tenant_id     = "00000000-0000-0000-0000-000000000001"
-  client_secret = "top-secret-password-string"
-}
-
 resource "azuredevops_project" "project" {
   name        = "Test Project"
   description = "Test Project Description"
