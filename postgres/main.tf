f777b4860b2990df1064fa01c844df250eae7574
Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>
2024-03-23 12:25:26 -0700

    Update main.tf

--- postgres/main.tf
+++ postgres/main.tf
@@ -90,6 +90,13 @@ provider "postgresql" {
   sslmode  = "disable"
 }
 
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [kubernetes_service.postgres]
+
+  create_duration = "60s"
+}
+
 resource postgresql_database "pg_db_test" {
+  depends_on = [time_sleep.wait_60_seconds]
   name = "pg_db_test"
 }
