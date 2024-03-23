3144c47a3751d8ed69642bec07b555176decac7f
Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>
2024-03-23 10:33:00 -0700

    Update main.tf

--- postgres/main.tf
+++ postgres/main.tf
@@ -11,6 +11,12 @@ terraform {
   }
 }
 
+variable "context" {
+  description = "This variable contains Radius recipe context."
+
+  type = any
+}
+
 variable "password" {
   description = "The password for the PostgreSQL database"
   type        = string
