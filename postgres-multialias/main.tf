7519fdf7438d5a6b8c4ca6d1bcd3ec1b1fb00bc7
Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>
2024-06-04 09:26:03 -0700

    adding multialias recipe

---
+++ postgres-multialias/main.tf
@@ -0,0 +1,154 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "1.16.0"
+      configuration_aliases = [postgresql.pgdb-test1, postgresql.pgdb-test2]
+    }
+  }
+}
+
+variable "context" {
+  description = "This variable contains Radius recipe context."
+  type = any
+}
+
+variable "password" {
+  description = "The password for the PostgreSQL database"
+  type        = string
+}
+
+resource "kubernetes_deployment" "postgres1" {
+  metadata {
+    name      = "postgres1"
+    namespace = var.context.runtime.kubernetes.namespace
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        app = "postgres1"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "postgres1"
+        }
+      }
+
+      spec {
+        container {
+          image = "postgres:latest"
+          name  = "postgres1"
+
+          env {
+            name  = "POSTGRES_PASSWORD"
+            value = var.password
+          }
+
+          port {
+            container_port = 5432
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "postgres1" {
+  metadata {
+    name      = "postgres1"
+    namespace = var.context.runtime.kubernetes.namespace
+  }
+
+  spec {
+    selector = {
+      app = "postgres1"
+    }
+
+    port {
+      port        = 5432
+      target_port = 5432
+    }
+  }
+}
+
+resource "kubernetes_deployment" "postgres2" {
+  metadata {
+    name      = "postgres2"
+    namespace = var.context.runtime.kubernetes.namespace
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        app = "postgres2"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "postgres2"
+        }
+      }
+
+      spec {
+        container {
+          image = "postgres:latest"
+          name  = "postgres2"
+
+          env {
+            name  = "POSTGRES_PASSWORD"
+            value = var.password
+          }
+
+          port {
+            container_port = 5432
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "postgres2" {
+  metadata {
+    name      = "postgres2"
+    namespace = var.context.runtime.kubernetes.namespace
+  }
+
+  spec {
+    selector = {
+      app = "postgres2"
+    }
+
+    port {
+      port        = 5432
+      target_port = 5432
+    }
+  }
+}
+
+resource "time_sleep" "wait_20_seconds" {
+  depends_on = [kubernetes_service.postgres1, kubernetes_service.postgres2]
+  create_duration = "20s"
+}
+
+resource "postgresql_database" "pg_db_test1" {
+  provider = postgresql.pgdb-test1
+  depends_on = [time_sleep.wait_20_seconds]
+  name = "pg_db_test1"
+}
+
+resource "postgresql_database" "pg_db_test2" {
+  provider = postgresql.pgdb-test2
+  depends_on = [time_sleep.wait_20_seconds]
+  name = "pg_db_test2"
+}
